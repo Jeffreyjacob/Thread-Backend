42e1f5073fbe13a6a0e7a7a92305acc032144737
Jeffreyjacob <jeffreyigiemeh16@gmail.com>
2024-07-07 06:57:04 -0700

    new commit

--- routes/instagramAuth.js
+++ routes/instagramAuth.js
@@ -5,6 +5,8 @@ import passport from 'passport'
 const router = express.Router()
 
 // authenticate the user using google
+//forward the request to google's authentication server
+router.route("/instagram").get(passport.authenticate('instagram'))
 
 router.route("/instagram/callback").get(passport.authenticate("instagram",{
     failureRedirect:`${process.env.FRONTEND_URL}/login`
@@ -16,7 +18,6 @@ router.route("/instagram/callback").get(passport.authenticate("instagram",{
     res.redirect(process.env.FRONTEND_URL);
 })
 
-//forward the request to google's authentication server
-router.route("/instagram").get(passport.authenticate("instagram"))
+
 
 export default  router
